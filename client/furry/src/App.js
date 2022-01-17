767327765b09d3255682fdb2fdfcfd9f1674b190
abbyyvan <499965776@qq.com>
2022-01-16 22:36:36 -0500

    login

--- client/furry/src/App.js
+++ client/furry/src/App.js
@@ -1,15 +1,60 @@
 import NavBar from './components/navbar'
-import Display from './components/display'
+//import Display from './components/display'
+import LoginForm from './components/LoginForm';
+import React, { useState } from 'react';
+
 
 
 function App() {
-  return (
-    <div>
-      <NavBar></NavBar>
-      <div style = {{padding:100}}></div>
-      
-    </div>
-  );
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin1234"
+  }
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  //this is a function to attempt login
+  const Login = details => {
+    console.log(details);
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Login succeed!");
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    } else {
+      console.log(" 账号或者密码不正确");
+      setError("账号或密码不正确")
+    }
+  }
+
+  const Logout = () => {
+    console.log("Logout")
+    setUser({name:"", email:""});
+
+  }
+
+    return (
+      <div className="Login">
+        {(user.email != "") ? (
+          <div className="welcome">
+            <h2> <span>{user.name}</span>, 欢迎来到furryshelter</h2>
+            <button onClick = {Logout}>退出账号</button>
+          </div>) : (
+          <LoginForm Login={Login} error={error}></LoginForm>
+        )
+        }
+      </div>
+    )
+  
+  // return (
+  //   <div>
+  //     <NavBar></NavBar>
+  //     <div style = {{padding:100}}></div>
+
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ client/furry/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React , {useState} from 'react'
+
+
+function LoginForm({Login, error}) {
+    const [details, setDetails] = useState({name: "", email:"", password:""});
+
+    const submitHandler = e => {
+        e.preventDefault();
+        Login(details);
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <h2>登录</h2>
+                {(error != "") ? (<div className='error'>{error}</div>) : ""}
+                <div className="form-group">
+                    <label htmlFor="name"> 姓名: </label>
+                    <input type="text" name='name' id='name' onChange={e => setDetails({...details, name:e.target.value})} value={details.name}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">邮箱: </label>
+                    <input type="text" name='email' id='email' onChange={e => setDetails({...details, email:e.target.value})} value={details.email}/>
+
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">密码: </label>
+                    <input type='password' name='password' id='password' onChange={e => setDetails({...details, password:e.target.value})} value={details.password}/>
+
+                </div>
+                <input type="submit" value = "确定登录"/>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
