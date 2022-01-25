e1cb0722a0b684f60968493235d35d341afa178b
abbyyvan <499965776@qq.com>
2022-01-25 06:56:22 -0500

    new pet post

--- client/furry/src/components/PetList.js
+++ client/furry/src/components/PetList.js
@@ -1,63 +1,86 @@
 import "./Pet.css";
 import NavBar from "./navbar";
 import Grid from "@material-ui/core/Grid";
+import pic1 from "./1.jpeg";
+import pic2 from "./2.jpeg";
+import pic3 from "./3.jpeg";
+import pic4 from "./4.jpeg";
+import pic5 from "./5.jpeg";
 
-const firstPet = {
-  name: "Lily",
-  age: "2 year old",
-  img: "https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-  story:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
-};
-
-const secondPet = {
-  name: "Jason",
-  age: "3 year old",
-  img: "https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-  story:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
-};
+const pets = [
+  {
+    name: "Lily",
+    age: "2 year old",
+    img: pic1,
+    story:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
+  },
+  {
+    name: "Jason",
+    age: "3 year old",
+    img: pic2,
+    story:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
+  },
+  {
+    name: "Abby",
+    age: "1 year old",
+    img: pic3,
+    story:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
+  },
+  {
+    name: "kate",
+    age: "2.5 year old",
+    img: pic4,
+    story:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
+  },
+  {
+    name: "Yvan",
+    age: "3 year old",
+    img: pic5,
+    story:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
+  },
+  {
+    name: "Bob",
+    age: "3 year old",
+    img: pic2,
+    story:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda necessitatibus quidem perspiciatis a nisi sequi eaque tenetur adipisci voluptatem error aliquid, iste molestias harum odio odit, fugiat, velit animi placeat.",
+  },
+];
 
 const Pet = (props) => {
-  console.log(props);
+  const { img, name, age, story } = props.pet;
   return (
-    <article className="pet">
-      <img src={props.img} alt="" className="pet"></img>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h4>{props.story}</h4>
-    </article>
+    <div>
+      <article className="pet">
+        <img src={img} alt="" className="pet"></img>
+        <h1>{name}</h1>
+        <h4>{age}</h4>
+        <p>{story}</p>
+      </article>
+    </div>
   );
 };
-
+const names = ["a", "asda", "adsa"];
+const newnames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
 function PetList() {
   return (
-    <section className="petlist">
-      <Pet
-        img={firstPet.img}
-        name={firstPet.name}
-        age={firstPet.age}
-        story={firstPet.story}
-      ></Pet>
-      <Pet
-        img={secondPet.img}
-        name={secondPet.name}
-        age={secondPet.age}
-        story={secondPet.story}
-      ></Pet>
-      <Pet
-        img={secondPet.img}
-        name={secondPet.name}
-        age={secondPet.age}
-        story={secondPet.story}
-      ></Pet>
-      <Pet
-        img={firstPet.img}
-        name={firstPet.name}
-        age={firstPet.age}
-        story={firstPet.story}
-      ></Pet>
-    </section>
+    <div>
+      <NavBar></NavBar>
+      <section className="petlist">
+        {pets.map((pet) => {
+          const { img, name, age, story } = pet;
+          // return <Pet img={img} name={name} age={age} story={story}></Pet>;
+          return <Pet pet={pet}></Pet>;
+        })}
+      </section>
+    </div>
   );
 }
 
